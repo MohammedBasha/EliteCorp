18dda6395ba33c390784825a89967ad06d4e57fc
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-09-16 04:23:09 +0200

    reorganize the files structure

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,22 @@ $(function () {
         upperH = $(".upper-bar").outerHeight(),
         navH = $(".navbar").outerHeight();
 
-    $(".main-slider, .carousel-item").height(winH - (upperH + navH));
+    $(".main-slider, .main-slider .carousel-item").height(winH - (upperH + navH));
+
+
+    // Initialize the Featured Work Filter
+    $(".featured-filter > *").click(function () {
+        $(this).addClass("active").siblings().removeClass("active");
+
+        if($(this).data("class") === 'all') {
+            $(".featured-image").css("opacity", 1);
+        } else {
+            $(".featured-image").css("opacity", .3);
+            $($(this).data("class")).css("opacity", 1);
+        }
+    });
+	
+	// Set the Testimonials Inner Content Margin top
+    $(".testimonials-slider .carousel-inner").css("margin-top", ($(".testimonials-slider").height() - $(".testimonials-slider .carousel-inner").height()) / 2);
 
 });
